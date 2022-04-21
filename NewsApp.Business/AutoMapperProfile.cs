6c291ecc6381e85728d5901d2e06dbbfe7580f32
Mhieu181120 <75715594+Mhieu181120@users.noreply.github.com>
2022-04-21 16:04:11 +0700

    setup automapper + swagger

---
+++ NewsApp.Business/AutoMapperProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using NewsApp.DataAccessor.Entities;
+using NewsApp.Contracts;
+using System;
+
+namespace NewsApp.Business
+{
+    public class AutoMapperProfile : AutoMapper.Profile
+    {
+        public AutoMapperProfile()
+        {
+            FromDataAccessorLayer();
+            FromPresentationLayer();
+        }
+
+        private void FromPresentationLayer()
+        {
+            
+        }
+
+        private void FromDataAccessorLayer()
+        {
+            
+        }
+    }
+}
--- NewsApp/Program.cs
+++ NewsApp/Program.cs
@@ -1,9 +1,12 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using NewsApp.DataAccessor.Data;
 using NewsApp.DataAccessor.Entities;
+using System.Reflection;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -32,6 +35,45 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 //Razor Pages
 builder.Services.AddRazorPages();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Rookie id4 Api",
+        Version = "v1"
+    });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                  {
+                    {
+                      new OpenApiSecurityScheme
+                      {
+                        Reference = new OpenApiReference
+                          {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                          },
+                          Scheme = "oauth2",
+                          Name = "Bearer",
+                          In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                      }
+                    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -40,11 +82,20 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
 
+    options.OAuthClientId("demo_api_swagger");
+    options.OAuthAppName("Demo API - Swagger");
+    options.OAuthUsePkce();
+});
 app.UseRouting();
 
 app.UseAuthentication();
